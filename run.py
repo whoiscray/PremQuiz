a0483949e196e14e44269aa68f769bddc5392173
cfconor <cfconor@gmail.com>
2021-03-19 13:56:09 +0000

    added base.html and index.html content, index.html now extends base.html

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
     import env
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
