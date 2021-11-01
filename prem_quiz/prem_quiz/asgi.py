8090c245552d957af67cafa023500e02e5c6b014
whoiscray <whoiscray@mailfence.com>
2021-11-01 18:22:10 +0000

    initialized new brahcn with venv and django folder

---
+++ prem_quiz/prem_quiz/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for prem_quiz project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prem_quiz.settings')
+
+application = get_asgi_application()
