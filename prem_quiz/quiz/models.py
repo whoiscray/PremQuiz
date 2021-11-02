35ecabff38b1173f455666c9e440dae0cc99522d
whoiscray <whoiscray@mailfence.com>
2021-11-02 16:35:10 +0000

    updated URLs, added first model class

--- prem_quiz/quiz/models.py
+++ prem_quiz/quiz/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model):
+    player_firstname = models.CharField(max_length=200)
+    player_lastname = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
