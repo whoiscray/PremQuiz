cbb49a2ddddd9e28bfe40c0c902b078966063263
whoiscray <whoiscray@mailfence.com>
2021-11-07 23:33:16 +0000

    added html for views

--- prem_quiz/quiz/views.py
+++ prem_quiz/quiz/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Player
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the quiz index.")
+    player_list = Player.objects.order_by('-pub_date')[:5]
+
+    context = {
+        'player_list': player_list,
+    }
+
+    return render(request, 'quiz/index.html', context)
