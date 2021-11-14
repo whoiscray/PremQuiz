63443cafa9b323cbb7f46da35860c34d3d19bc91
whoiscray <whoiscray@mailfence.com>
2021-11-14 13:30:52 +0000

    updated model and gitignore file

--- prem_quiz/quiz/models.py
+++ prem_quiz/quiz/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class Player(models.Model):
     player_firstname = models.CharField(max_length=200)
     player_lastname = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField().auto_now_add
