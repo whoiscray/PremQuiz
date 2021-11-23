d229b3d32dbe3c4fe8c9befb051cfabc82d6d972
whoiscray <whoiscray@mailfence.com>
2021-11-23 18:11:57 +0000

    added goals and appearances models

--- prem_quiz/quiz/models.py
+++ prem_quiz/quiz/models.py
@@ -8,8 +8,26 @@ class Player(models.Model):
     player_lastname = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 class PlayerForm(ModelForm):
     class Meta:
         model = Player
         fields = ['player_firstname','player_lastname']
 
+class Season(models.Model):
+    season_year = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class Goals(models.Model):
+    season_id = models.ForeignKey(Season, on_delete=models.DO_NOTHING)
+    player_id = models.ForeignKey(Player, on_delete=models.DO_NOTHING)
+    goals = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class Appearances(models.Model):
+    season_id = models.ForeignKey(Season, on_delete=models.DO_NOTHING)
+    player_id = models.ForeignKey(Player, on_delete=models.DO_NOTHING)
+    appearances = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
